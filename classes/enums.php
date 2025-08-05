63f979b7bc3af6a3727b6bb862448239e4131821
BisoyJan <bisoyjan@gmail.com>
2025-08-05 22:29:46 +0800

    Add enums for DaysOfWeek and Colour with utility function for weekend check

---
+++ classes/enums.php
@@ -0,0 +1,37 @@
+<?php
+
+// enums can let you create a new type with some fixed set of possible values.
+// basically, enums prevents you from invalid values being used.
+
+enum DaysOfWeek
+{
+    case MONDAY;
+    case TUESDAY;
+    case WEDNESDAY;
+    case THURSDAY;
+    case FRIDAY;
+    case SATURDAY;
+    case SUNDAY;
+}
+
+$today = DaysOfWeek::WEDNESDAY;
+
+if ($today === DaysOfWeek::WEDNESDAY) {
+    echo "It is Wed!\n";
+}
+
+enum Colour: string
+{
+    case RED = '#FF0000';
+    case GREEN = '#00FF00';
+    case BLUE = '#0000FF';
+}
+
+echo Colour::RED->value;
+
+function isWeekend(DaysOfWeek $day): bool
+{
+    return $day === DaysOfWeek::SATURDAY || $day === DaysOfWeek::SUNDAY;
+}
+
+echo isWeekend(DaysOfWeek::MONDAY) ? 'Yes' : 'NO';
